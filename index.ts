58abfdfd1f4c411ebf4ab5da3f65f5cf470cac93
floofboy <floofiestboy@gmail.com>
2023-01-20 18:11:16 -0800

    See if render deploy succeeds with a fake app

--- index.ts
+++ index.ts
@@ -1,6 +1,26 @@
 import { Client, Intents } from "discord.js";
 import { Bot } from "./structs/Bot";
 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (_req: any, res: any) => res.type('html').send(html));
+
+app.listen(port, () => console.log(`Render workaround listening on port ${port}!`));
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>A title</title>
+  </head>
+  <body>
+    <p>let's see if this works</p>
+  </body>
+</html>
+`;
+
 export const bot = new Bot(
   new Client({
     restTimeOffset: 0,
